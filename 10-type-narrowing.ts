739dde4dd17efdd6fe9cf71c6a2d81be98b94496
Kthalv <alvaradokathline@gmail.com>
2026-03-30 20:31:24 +0800

    Done playing and updating the 10-type-narrowing.ts file

--- 10-type-narrowing.ts
+++ 10-type-narrowing.ts
@@ -1,12 +1,15 @@
+// Access HTML input element
 const inputEl = document.getElementById('user-name')
 
-console.log(inputEl.value) // This line is unsafe because inputEl might be null
-
+// Type narrowing: check if element exists
 if (!inputEl) {
     throw new Error('Element not found!');
 }
 
-console.log(inputEl.value)
+// Type casting to HTMLInputElement
+const inputValue = (inputEl as HTMLInputElement).value
+
+console.log("Input value:", inputValue)
 
 // You can convince TypeScript that you are sure the value will not be null by using the non-null assertion operator `!`
 // You can use the optional chaining `?.` operator to safely check if value is not null before the next operation
