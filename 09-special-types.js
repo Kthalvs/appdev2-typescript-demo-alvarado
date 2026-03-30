9797dfa25b0facdff4f695e1807244b02e0b7731
Kthalv <alvaradokathline@gmail.com>
2026-03-30 20:03:27 +0800

    Done playing and updating the 09-special-types.ts file

---
+++ 09-special-types.js
@@ -0,0 +1,22 @@
+"use strict";
+// Special Type - null
+let a;
+a = null;
+// a = "Hi"
+let b;
+b = null;
+b = "Hello";
+// b = 10
+// Special Type - undefined
+let c;
+c = undefined;
+// c = null
+// c = "Hi"
+let d;
+d = undefined;
+d = "Hi";
+// d = null
+// d = 10
+// Display values
+console.log("Value of b:", b);
+console.log("Value of d:", d);
--- 09-special-types.ts
+++ 09-special-types.ts
@@ -1,25 +1,25 @@
 // Special Type - null
 let a: null;
-
 a = null
 // a = "Hi"
 
 let b: null | string
-
 b = null
-b = "Hi"
+b = "Hello"
 // b = 10
 
 // Special Type - undefined
 let c: undefined;
-
 c = undefined
 // c = null
 // c = "Hi"
 
 let d: undefined | string
-
 d = undefined
 d = "Hi"
 // d = null
 // d = 10
+
+// Display values
+console.log("Value of b:", b)
+console.log("Value of d:", d)
